34e5ee6a78b406f767c4645afa8bdc9afc7ef247
yaswen <yaswen@163.com>
2018-05-28 22:27:58 +0800

    新建第五题文件。

---
+++ codeM/src/codeM/Main180005.java
@@ -0,0 +1,11 @@
+package codeM;
+
+import java.util.Scanner;
+
+public class Main180005 {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+	}
+	
+}
