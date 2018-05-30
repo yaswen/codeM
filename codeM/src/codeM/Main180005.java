5690d2fd1a42e663b40b9762c372360872e7e898
yaswen <yaswen@163.com>
2018-05-30 22:56:59 +0800

    新增空代码

--- codeM/src/codeM/Main180005.java
+++ codeM/src/codeM/Main180005.java
@@ -1,7 +1,14 @@
 package codeM;
 
 import java.util.Scanner;
+//第一行，两个正整数n, m。n表示城市数量，m表示当天不同班次的火车数量。
+//接下来m行，每行3个整数x, y, c加两个字符串ts, td，均以空格作为分隔，表示当天的某一班火车。其中x, y, c, ts, td的含义如前描述。
+//所有的车次都是当天的，没有隔夜的票。
+//2 <= n <= 500, m <= 15000, c <= 1000, ts < td，所有数均为正整数。
+//车次保证不过夜，时间范围0:00, 0:30, 1:00, … , 23:00, 23:30, 24:00，可能存在重复车次。
 
+
+//一个整数，表示存在补救措施的前提下，小A到达C城的最小花费。如果不存在这样的路径，则输出-1。
 public class Main180005 {
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
